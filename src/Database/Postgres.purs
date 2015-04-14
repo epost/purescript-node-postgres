f46580200d32d6db6506629f289e2d5064136773
Antti Holvikari <anttih@gmail.com>
2015-04-14 09:20:35 +0300

    Use purescript-aff
    This changes all effects to use asynchronous effects (`Control.Monad.Aff`).
    This has several benefits:
    
    * the types become simpler because we don't have to use ContT,
    * we get simpler error handling, since Aff has errors built-in.
    
    Other things:
    
    * Foreign errors are now also lifted to Aff errors. This means the errors
      won't be in the type, but they will surface when you run the Aff. I think
      this is a fair compromise to make it easy to get to the results.
    
    * Added `execute`, `query`, `queryOne` and `queryValue`. See module docs for what
      they do.
    
    * Renamed `endClient` to just `end`.
    
    * Removed Database.Util since it's no longer needed.

--- src/Database/Postgres.purs
+++ src/Database/Postgres.purs
@@ -1,22 +1,36 @@
-module Database.Postgres where
+module Database.Postgres
+  ( Query(..)
+  , Client()
+  , DB()
+  , ConnectionInfo()
+  , connect
+  , end
+  , execute
+  , query
+  , queryOne
+  , queryValue
+  , withConnection
+  ) where
 
 import Control.Alt
 import Control.Monad.Eff
-import Control.Monad.Cont.Trans
 import Control.Monad.Trans
 import Data.Either
 import Data.Array
 import Data.Foreign
 import Data.Foreign.Class
-import Util (readSingularProperty)
+import Data.Maybe
+import Control.Monad.Aff
+import Control.Monad.Eff.Class
+import Control.Monad.Eff.Exception(Error(), error)
+import Control.Monad.Error.Class (throwError)
+import Data.Traversable (sequence)
 
 newtype Query a = Query String
 
 foreign import data Client :: *
 
 foreign import data DB :: !
-
-type DBEff eff = Eff (db :: DB | eff)
 
 type ConnectionInfo =
   { host :: String
@@ -26,81 +40,107 @@ type ConnectionInfo =
   , password :: String
   }
 
+-- | Makes a connection to the database.
+connect :: forall eff. ConnectionInfo -> Aff (db :: DB | eff) Client
+connect ci = connect'
+  $ "postgres://"
+  <> ci.user <> ":"
+  <> ci.password <> "@"
+  <> ci.host <> ":"
+  <> show ci.port <> "/"
+  <> ci.db
 
--- Low-level API. --------------------------------------------------------------
+-- | Runs a query and returns nothing.
+execute :: forall eff a. Query a -> Client -> Aff (db :: DB | eff) Unit
+execute (Query sql) client = void $ runQuery sql client
 
-connect :: forall eff. ConnectionInfo -> DBEff eff Client
-connect ci = connectJS $ "postgres://" <> ci.user <> ":" <> ci.password <> "@" <> ci.host <> ":" <> show ci.port <> "/" <> ci.db
+-- | Runs a query and returns all results.
+query :: forall eff a. (IsForeign a) => Query a -> Client -> Aff (db :: DB | eff) [a]
+query (Query sql) client = do
+  rows <- runQuery sql client
+  either liftError pure (sequence $ read <$> rows)
 
-runQuery :: forall row eff. (IsForeign row) =>
-            Query row -> Client -> ([F row] -> DBEff eff Unit) -> DBEff eff Unit
-runQuery (Query query) client handleRows = runQueryRowsForeignRaw query client (map deserialize >>> handleRows)
-  where deserialize foreignVal = read foreignVal <|> readSingularProperty foreignVal
+-- | Runs a query and returns the first row, if any
+queryOne :: forall eff a. (IsForeign a) => Query a -> Client -> Aff (db :: DB | eff) (Maybe a)
+queryOne (Query sql) client = do
+  rows <- runQuery sql client
+  maybe (pure Nothing) (either liftError (pure <<< Just)) $ read <$> (rows !! 0)
 
+-- | Runs a query and returns a single value, if any.
+queryValue :: forall eff a. (IsForeign a) => Query a -> Client -> Aff (db :: DB | eff) (Maybe a)
+queryValue (Query sql) client = do
+  val <- runQueryValue sql client
+  either liftError (pure <<< Just) $ read val
 
-foreign import connectJS """
-  function connectJS(conString) {
-    return function() {
+-- | Connects to the database, calls the provided function with the client
+-- | and returns the results.
+withConnection :: forall eff a
+  . ConnectionInfo
+  -> (Client -> Aff (db :: DB | eff) a)
+  -> Aff (db :: DB | eff) a
+withConnection info p = do
+  client <- connect info
+  finally (p client) $ liftEff (end client)
+
+liftError :: forall e a. ForeignError -> Aff e a
+liftError err = throwError $ error (show err)
+
+finally :: forall eff a. Aff eff a -> Aff eff Unit -> Aff eff a
+finally a sequel = do
+  res <- attempt a
+  sequel
+  either throwError pure res
+
+foreign import connect' """
+  function connect$prime(conString) {
+    return function(success, error) {
       var pg = require('pg');
       var client = new pg.Client(conString);
+      client.connect(function(err) {
+        if (err) {
+          error(err);
+        } else {
+          success(client);
+        }
+      })
       return client;
+    }
+  }
+  """ :: forall eff. String -> Aff (db :: DB | eff) Client
+
+foreign import runQuery """
+  function runQuery(queryStr) {
+    return function(client) {
+      return function(success, error) {
+        client.query(queryStr, function(err, result) {
+          if (err) {
+            error(err);
+          } else {
+            success(result.rows);
+          }
+        })
+      };
     };
   }
-""" :: forall eff. String -> DBEff eff Client
+  """ :: forall eff. String -> Client -> Aff (db :: DB | eff) [Foreign]
 
-
-foreign import connectClient """
-  function connectClient(client) {
-    return function() {
-      client.connect();
+foreign import runQueryValue """
+  function runQueryValue(queryStr) {
+    return function(client) {
+      return function(success, error) {
+        client.query(queryStr, function(err, result) {
+          if (err) return error(err);
+          success(result.rows.length > 0 ? result.rows[0][result.fields[0].name] : undefined);
+        })
+      };
     };
   }
-""" :: forall eff. Client -> DBEff eff Unit
+  """ :: forall eff. String -> Client -> Aff (db :: DB | eff) Foreign
 
-foreign import endClient """
-  function endClient(client) {
+foreign import end """
+  function end(client) {
     return function() {
       client.end();
     };
   }
-""" :: forall eff. Client -> DBEff eff Unit
-
-
-foreign import runQueryRowsForeignRaw """
-  function runQueryRowsForeignRaw(queryStr) {
-      return function (client) {
-        return function(handleRows) {
-            return function() {
-              client.query(queryStr, function(err, result) {
-                if (err) {
-                  console.error('error running query', err);
-                } else {
-                  handleRows(result.rows)();
-                }
-              });
-            };
-          };
-        };
-  }
-  """ :: forall eff eff'. String -> Client -> ([Foreign] -> eff') -> DBEff eff Unit
-
-
--- Continuation-based API. -----------------------------------------------------
-
-runQueryCont :: forall eff a. (IsForeign a) => Query a -> Client -> ContT Unit (DBEff eff) [F a]
-runQueryCont query client = ContT $ runQuery query client
-
-runQueryCont_ :: forall eff. Query Unit -> Client -> ContT Unit (DBEff eff) [F Unit]
-runQueryCont_ query client = ContT $ runQuery query client
-
-withConnectionCont :: forall eff a. ConnectionInfo -> (Client -> ContT Unit (DBEff eff) a) -> ContT Unit (DBEff eff) a
-withConnectionCont connectionInfo dbProg = do
-  client <- lift $ connect connectionInfo
-  lift $ connectClient client
-  res <- dbProg client
-  lift $ endClient client
-  return res
-
--- needed for runQuery_
-instance unitIsForeign :: IsForeign Unit where
-  read _ = Right unit
+  """ :: forall eff. Client -> Eff (db :: DB | eff) Unit
